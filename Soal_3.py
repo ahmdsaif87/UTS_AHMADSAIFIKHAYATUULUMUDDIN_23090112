fd368e467fb89c7d870876749b1902d30fec4c67
ahmadsaif7456@gmail.com <ahmadsaif7456@gmail.com>
2024-05-07 16:54:58 +0700

    okok

--- Soal_3.py
+++ Soal_3.py
@@ -1,9 +1,7 @@
-jumlahBarang = input("Masukkan Jumlah Barang: ")
-hargaBarang1 = int(input("Masukkan Harga Barang Pertama: "))
-hargaBarang2 = int(input("Masukkan Harga Barang Kedua: "))
-totalHarga = hargaBarang1 + hargaBarang2
-
-print(f"Jumlah Barang: {jumlahBarang}")
-print(f"Harga Barang Pertama: {hargaBarang1}")
-print(f"Harga Barang Kedua: {hargaBarang2}")
-print(f"Total Belanjaan: Rp.{totalHarga}")
+jumlahBarang = int(input("Masukkan Jumlah Barang: "))
+total = 0
+for i in range (jumlahBarang):
+    harga = int(input("Masukkan Harga: "))
+    total += harga
+    
+print(f"Total Harga : {total}")
