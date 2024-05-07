e425ef0880ba78a47afa76b4f912b97ba04e6331
ahmadsaif7456@gmail.com <ahmadsaif7456@gmail.com>
2024-05-07 16:34:40 +0700

    okok

--- Soal_3.py
+++ Soal_3.py
@@ -6,4 +6,4 @@ totalHarga = hargaBarang1 + hargaBarang2
 print(f"Jumlah Barang: {jumlahBarang}")
 print(f"Harga Barang Pertama: {hargaBarang1}")
 print(f"Harga Barang Kedua: {hargaBarang2}")
-print(f"Total Belanjaan: {totalHarga}")
+print(f"Total Belanjaan: Rp.{totalHarga}")
---
+++ Soal_4tampilan.py
@@ -0,0 +1,2 @@
+import Soal_4 as so
+so.perhitunganBMI()
